071e7649beccb93265c3b0971ca5d58b71ff400e
SongPro <ovoxinsong@gmail.com>
2023-12-06 10:40:53 +0800

    新增ErcerLinks插件pjax兼容。

--- data/js/ErcerLinks.js
+++ data/js/ErcerLinks.js
@@ -8,11 +8,11 @@ const submitLink = (() => {
 
     return formId => {
         count++;
-        if (count > threshold) {
+        if (count >= threshold) {
             const currentTime = new Date().getTime(); // 获取当前时间戳
             if (currentTime - lastCallTime < limitTime) { // 如果当前时间与上次调用时间小于限制时间
                 const remainingTime = Math.floor((lastCallTime + limitTime - currentTime) / 1000);
-                showMessage('频繁提交','请等待 ' + remainingTime + ' 秒后再尝试提交','warning');
+                showMessage('频繁提交','请等待 ' + remainingTime + ' 秒后再尝试提交','error');
                 return;
             }
             count = 0;
@@ -32,21 +32,22 @@ const submitLink = (() => {
         });
     };
 })();
-
-$(document).ready(() => {
-    $('#postLink').html(`
-        <form style="text-align: center;" class="form-inline panel-body" role="form" id="F-link">
-            <h4>💞申请友链💞</h4>
-            <div class="form-group"> <input type="text" name="host_name" class="form-control" id="host_name" placeholder="站点名称" required=""></div>
-            <div class="form-group"> <input type="url" name="host_url" class="form-control" id="host_url" placeholder="站点链接" required=""></div>
-            <div class="form-group"> <input type="url" name="host_png" class="form-control" id="host_png" placeholder="站点图标" required=""></div>
-            <div class="form-group"> <input type="text" name="host_msg" class="form-control" id="host_msg" placeholder="站点描述（不用太长）" required=""></div>
-            <div class="checkbox m-l m-r-xs"><label class="i-checks"><input type="checkbox" name="host_yes" required=""><i></i>已添加本站为友链</label>
-                <button class="btn btn-danger" type="submit">申请</button>
-            </div>
-        </form>`);
-    $('#F-link').submit(event => {
-        event.preventDefault();
-        submitLink('F-link');
+function pjax_Link() {
+    $(document).ready(() => {
+        $('#postLink').html(`
+            <form style="text-align: center;" class="form-inline panel-body" role="form" id="F-link">
+                <h4>💞申请友链💞</h4>
+                <div class="form-group"> <input type="text" name="host_name" class="form-control" id="host_name" placeholder="站点名称" required=""></div>
+                <div class="form-group"> <input type="url" name="host_url" class="form-control" id="host_url" placeholder="站点链接" required=""></div>
+                <div class="form-group"> <input type="url" name="host_png" class="form-control" id="host_png" placeholder="站点图标" required=""></div>
+                <div class="form-group"> <input type="text" name="host_msg" class="form-control" id="host_msg" placeholder="站点描述（不用太长）" required=""></div>
+                <div class="checkbox m-l m-r-xs"><label class="i-checks"><input type="checkbox" name="host_yes" required=""><i></i>已添加本站为友链</label>
+                    <button class="btn btn-danger" type="submit">申请</button>
+                </div>
+            </form>`);
+        $('#F-link').submit(event => {
+            event.preventDefault();
+            submitLink('F-link');
+        });
     });
-});
+}pjax_Link();
